2921ef61d034606df229773a64090e7b78dd1637
alexey17kos <93329334+alexey17kos@users.noreply.github.com>
2021-11-22 20:03:39 +0300

    сделал функционал из урока 16

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
+// объявление переменных
 const btnCart = document.getElementById('cart');
 const modal = document.querySelector('.modal');
 const btnClose = document.querySelector('.btn-close');
+const rows = modal.querySelectorAll('.row');
+const total = modal.querySelector('.modul-sum');
 
+// объявление функций
 const modalOpen = () => {
     modal.classList.toggle('modal--close');
 };
+
+const getFullPrice = () => {
+    let fullPrice = 0;
+    rows.forEach(row => {
+        let priceBlock = row.querySelector('.price')
+        let price = +priceBlock.textContent
+        fullPrice += price;
+    })
+    total.textContent = fullPrice
+}
+// вызов функций
+getFullPrice()
 
 btnCart.addEventListener('click', modalOpen);
 btnClose.addEventListener('click', modalOpen);
@@ -15,4 +31,32 @@ modal.addEventListener('click', (event) => {
     }
 });
 
-console.log();
+rows.forEach(row => {
+    let newPrice = 0;
+    let priceBlock = row.querySelector('.price')
+    let price = +priceBlock.textContent
+    let countBlock = row.querySelector('.count')
+    let count = countBlock.textContent
+    const btnMinus = row.querySelector('.minus')
+    const btnPlus = row.querySelector('.plus')
+
+    const getNewPrice = (count, price) => {
+        newPrice = count * price
+        priceBlock.textContent = newPrice
+        getFullPrice()
+    }
+    
+    btnMinus.addEventListener('click', () => {
+        if (count > 0) {
+        count--
+        countBlock.textContent = count
+        getNewPrice(count, price)
+        }
+    })
+    btnPlus.addEventListener('click', () => {
+        count++
+        countBlock.textContent = count
+        getNewPrice(count, price)
+    })
+})
+//логеры
