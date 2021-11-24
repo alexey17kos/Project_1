7f39b35dc6e27d5fad6bfded5ba4bd0792a1c1d9
alexey17kos <93329334+alexey17kos@users.noreply.github.com>
2021-11-24 19:41:29 +0300

    урок 18 завершен

--- js/main.js
+++ js/main.js
@@ -5,12 +5,23 @@ const modal = document.querySelector('.modal');
 const btnClose = document.querySelector('.btn-close');
 const rows = modal.querySelectorAll('.row');
 const total = modal.querySelector('.modal-sun');
-
+const mainIndex = document.getElementById('index');
+const mainRest = document.getElementById('restourant');
+const cardsBlockIndex = mainIndex.querySelector('.cards');
+const cardsIndex = cardsBlockIndex.querySelectorAll('.card');
+const logos = document.querySelectorAll('.link-logo');
+const cardsBlockRest = mainRest.querySelector('.cards');
+const cardsRest = cardsBlockRest.querySelectorAll('.card');
 // объявление функций
+const getProductModal = (nameProduct, priceProduct, index = 0) => {
+    let nameProductBlock = rows[index].querySelector('.product-name');
+    let priceProductBlock = rows[index].querySelector('.price');
+    nameProductBlock.textContent = nameProduct;
+    priceProductBlock.textContent = priceProduct;
+};
 const modalOpen = () => {
     modal.classList.toggle('modal--close');
 };
-
 const getFullPrice = () => {
     let fullPrice = 0;
     rows.forEach(row => {
@@ -20,18 +31,18 @@ const getFullPrice = () => {
     });
     total.textContent = fullPrice;
 };
+const getToMain = () => {
+    mainRest.classList.toggle('not--active');
+    mainIndex.classList.toggle('not--active');
+};
 // вызов функций
-getFullPrice();
-
 btnCart.addEventListener('click', modalOpen);
 btnClose.addEventListener('click', modalOpen);
-
 modal.addEventListener('click', (event) => {
     if (event.target == modal) {
         modalOpen();
     }
 });
-
 rows.forEach(row => {
     let newPrice = 0;
     let priceBlock = row.querySelector('.price');
@@ -46,7 +57,6 @@ rows.forEach(row => {
         priceBlock.textContent = newPrice;
         getFullPrice();
     };
-    
     btnMinus.addEventListener('click', () => {
         if (count > 0) {
         count--;
@@ -60,4 +70,25 @@ rows.forEach(row => {
         getNewPrice(count, price);
     });
 });
+cardsRest.forEach((card, index) => {
+    let nameProductBlock = card.querySelector('.card-title');
+    let priceProductBlock = card.querySelector('.product-price');
+    let nameProduct = nameProductBlock.textContent;
+    let priceProduct = +priceProductBlock.textContent.slice(0, 3);
+    getProductModal(nameProduct, priceProduct, index);
+});
+cardsIndex.forEach(card => {
+    card.addEventListener('click', () => {
+        getToMain();
+    });
+});
+logos.forEach(logo => {
+    logo.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (mainIndex.classList.contains('not--active')) {
+            getToMain();
+        }
+    });
+});
+getFullPrice();
 //логеры
